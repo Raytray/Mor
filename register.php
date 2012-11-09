f4e949ce8f2d4fa3da565dfc209b440df19b77fb
U-Richard-PC\Richard <Richard@Richard-PC.(none)>
2012-11-09 03:09:04 -0500

    message passing and password change

--- register.php
+++ register.php
@@ -5,25 +5,25 @@ mysql_select_db('a6519930_base') or die (mysql_error());
 if(mysql_num_rows(mysql_query("SELECT * from users WHERE email='" . $_POST['email'] . "'")) == 1){
 }
 else if($_POST['password'] != $_POST['retype-password']){
-    echo "The two passwords don't match!";
+    $_SESSION['message'] =  "The two passwords don't match!";
 }
 else if(strlen($_POST['email']) > 25){
-    echo "Email is too long!";
+    $_SESSION['message'] =  "Email is too long!";
 }
 else if(strlen($_POST['email']) < 6){
-    echo "Email is too short!";
+    $_SESSION['message'] =  "Email is too short!";
 }
 else if(strlen($_POST['password']) > 25){
-    echo "Password is too long!";
+    $_SESSION['message'] =  "Password is too long!";
 }
 else if(strlen($_POST['password']) < 6){
-    echo "Password is too short!";
+    $_SESSION['message'] =  "Password is too short!";
 }
 //else if(preg_match('/[^0-9A-Za-z]/',$_POST['email'])){
 //echo "Invalid characters in email!";
 //}
 else if(preg_match('/[^0-9A-Za-z]/',$_POST['password'])){
-    echo "Invalid characters in password!";
+    $_SESSION['message'] =  "Invalid characters in password!";
 }
 else{
     $email=$_POST['email'];
@@ -38,7 +38,8 @@ else{
     mysql_query("INSERT into users VALUES ('$email', '$password', '$salt', 'admin')") or die(mysql_error());
     $_SESSION['name'] = $email;
     $_SESSION['loggedIn']=true;
-
+    $_SESSION['message'] = "Thanks for registring!";
+    
     mail('$email', 'WELCOME!', 'NO REALLY! WE WELCOME YOU!!!');
 
     header("location:login.php");
