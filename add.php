2dfe3945f71b2e0fe3d6cee146dff124a934b1b3
Raymond Tang <Raytray25@gmail.com>
2012-11-09 03:27:59 -0500

    Now its broken..

--- add.php
+++ add.php
@@ -5,15 +5,14 @@ session_start();
 if($_SESSION['isAdmin'] == 1){
 	$courseshort = $_POST['courseshortname'];
 	$courselong = $_POST['courselongname'];
-	$major = $_POST['major'];
+	$majors = $_POST['majors'];
 	
-	mysql_query("INSERT into courses VALUES ('$courseshort', '$courselong', '$major')") or die(mysql_error());
+	
+	mysql_query("INSERT into courses VALUES ('$courseshort', '$courselong', '$majors')") or die(mysql_error());
 
     header("location:add.html");
 }
 else{
-echo $_SESSION['isAdmin'];
-echo $_SESSION['email'];
 echo 'ERROR 403, you must be admin';
 }
 ?>
