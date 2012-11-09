de99001393d1db603918928195ffb682f4924c03
U-Richard-PC\Richard <Richard@Richard-PC.(none)>
2012-11-09 04:32:10 -0500

    adding php's

--- login.php
+++ login.php
@@ -28,7 +28,7 @@
                 </ul>
                 </li>
                 <li><a href="links.html">Useful Links</a></li>
-                <li class="current_page_item"><a href="login.html">Login</a></li>
+                <li class="current_page_item"><a href="login.php">Login</a></li>
                 <li class="last"><a href="about.html">About</a></li>
             </ul>
             <div id="page"> 
@@ -40,7 +40,7 @@
                         if($_SESSION['loggedIn']!=true){
                         ?>
                         <form action="signin.php" method="post"><br />
-                            <b style="font-size:150%;">Log in</b><br />
+                            <b style="font-size:150%;">Account</b><br />
                             Email: <input type="email" name="email"/><br />
                             Password: <input type="password" name="password"/><br />
                             <input type="submit" value="Log in"/>
@@ -58,7 +58,7 @@
                         </form>
                         <?php
                         }
-                        { ?>
+                        else{ ?>
                         <form action="logout.php" method="post"><br/>
                           <input type="submit" value="Log Out" />
                         </form>
@@ -80,46 +80,8 @@
                     </div>
                 </div>
 
-                <div id="sidebar">
-                    <ul>
-                        <li>
-                        <div id="search" >
-                            <input type="text" placeholder="search..." name="s" id="search-text"/>
-                            <input type="submit" id="search-submit" value="GO"/>
-                        </div>
-                        </li>
-                        <li>
-                        <h2>Popular Majors</h2>
-                        <ol>
-                            <li><a href="#">B.S. Computer Science</a></li>
-                            <li><a href="#">B.A. Music</a></li>
-                            <li><a href="#">B.A. Economics</a></li>
-                            <li><a href="#">B.S. Biology</a></li>
-                            <li><a href="#">B.S. Mechanical Engineering</a></li>
-                            <li><a href="#">B.A. Art History</a></li>
-                        </ol>
-                        </li>
-                        <li>
-                        <h2>Popular Minors</h2>
-                        <ol>
-                            <li><a href="#">Engineering Business</a></li>
-                            <li><a href="#">Economics</a></li>
-                            <li><a href="#">Architecture</a></li>
-                            <li><a href="#">History</a></li>
-                            <li><a href="#">Chinese</a></li>
-                            <li><a href="#">Computer Science</a></li>
-                        </ol>
-                        </li>
-                    </ul>
-                    <img id="slideImg" src="Images/01.jpg" width="275" height="165" alt="Slide show" />
-
-                </div>
-            </div>
-            <div id="footer">
-                <p>This is a website created by Richard Li and Raymond Tang for CS 4753 ECommerce</p>
-                <p>Copyright (c) 2012. All rights reserved.</p>
-            </div>
+            <?php include 'sidefooter.php'; ?>
+    <!-- end #footer -->
         </div>
     </body>
-    <!-- end #footer -->
 </html>
