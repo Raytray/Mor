41f4bca32a2f28d291adb1f286a57dca2720a727
Raymond Tang <Raytray25@gmail.com>
2012-11-09 05:12:04 -0500

    I think delete works...

--- delete.php
+++ delete.php
@@ -2,7 +2,7 @@
 session_start();
 mysql_connect('mysql6.000webhost.com', 'a6519930_user', 'ecommerce2012') or die (mysql_error());
 mysql_select_db('a6519930_base') or die (mysql_error());
-if(mysql_num_rows(mysql_query("SELECT * from users WHERE email='" . $_POST['email'] . "'")) == 1){
+if(mysql_num_rows(mysql_query("SELECT * from users WHERE email='" . $_SESSION['name'] . "'")) == 1){
 }
 
     $password=$_POST['password'];
@@ -11,14 +11,15 @@ if(mysql_num_rows(mysql_query("SELECT * from users WHERE email='" . $_POST['emai
     $email=mysql_real_escape_string($email);
     $password=mysql_real_escape_string($password);
     $password=crypt($password,$email);//hash the password
+    if(mysql_num_rows(mysql_query("Select * FROM users WHERE email='$email' and password='$password'"))==1){
+    mysql_query("DELETE FROM users WHERE email = '$_SESSION['name']") or die(mysql_error());
 
-    mysql_query("INSERT into users VALUES ('$email', '$password', 'undeclared' '0')") or die(mysql_error());
-    $_SESSION['name'] = $email;
-    $_SESSION['loggedIn']=true;
-    $_SESSION['message'] = "Thanks for registring!";
-    
-    mail('$email', 'WELCOME!', 'NO REALLY! WE WELCOME YOU!!!');
-
-    header("location:login.php");
+    session_destroy();
+    header("location:index.php");
+}
+else{
+else{
+header("location:login.php");
+}
 }
 ?>
