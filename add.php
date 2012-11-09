2c2b3ce2f31ba8b7d67466016dc4a28d4cbe8d23
Raymond Tang <Raytray25@gmail.com>
2012-11-09 01:42:25 -0500

    think add should work now.. testing now

--- add.php
+++ add.php
@@ -1,3 +1,13 @@
 <?php
-if($_session['isAdmin'] == true;
+if($_session['isAdmin'] == 1){
+	$courseshort = $_POST['courseshortname'];
+	$coursenumber = $_POST['coursenumber'];
+	$courselong = $_POST['courselongname'];
+	$major = $_POST['major'];
+	
+	mysql_query("INSERT into courses VALUES ('$courseshort' '$coursenumber', '$courselong', '$major', '', '', '')") or die(mysql_error());
+
+    header("location:add.html");
+}
+
 ?>
