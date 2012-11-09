a71c9fd812eb0cbf19cbee1e4b81ac1da75dcf9f
Raymond Tang <Raytray25@gmail.com>
2012-11-09 01:48:32 -0500

    successfully keeps you out if not admin... now will it reject if admin questionmark

--- add.php
+++ add.php
@@ -1,13 +1,14 @@
 <?php
 if($_session['isAdmin'] == 1){
 	$courseshort = $_POST['courseshortname'];
-	$coursenumber = $_POST['coursenumber'];
 	$courselong = $_POST['courselongname'];
 	$major = $_POST['major'];
 	
-	mysql_query("INSERT into courses VALUES ('$courseshort' '$coursenumber', '$courselong', '$major', '', '', '')") or die(mysql_error());
+	mysql_query("INSERT into courses VALUES ('$courseshort', '$courselong', '$major', '', '', '')") or die(mysql_error());
 
     header("location:add.html");
 }
-
+else{
+echo 'ERROR 403, you must be admin';
+}
 ?>
