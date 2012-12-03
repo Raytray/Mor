98f42f73964c3610bc4db6d69afff58a531f3cc8
U-Richard-Envy\Richard Li <Richard Li@Richard-Envy.(none)>
2012-12-03 00:15:08 -0500

    base logic part 1

---
+++ majorMatch.php
@@ -0,0 +1,40 @@
+<?php
+class major
+{
+    public $name;
+    public $courseID = new array();
+    public $courseName = new array();
+}
+$myMajor = $_GET["myMajor"];
+$majors = new array();
+
+mysql_connect('mysql6.000webhost.com', 'a6519930_user', 'ecommerce2012')
+    or die ("Could not connect to mysql because ".mysql_error());
+mysql_select_db('a6519930_base')
+    or die("Could not select database because ".mysql_error());
+
+$result = mysql_query("SELECT * from courses");
+while($row = mysql_fetch_array($result)){
+    $found = false;
+    $majorList = explode(", ", $row['majors']);
+    foreach($majorList as $i){
+       if($i == $myMajor) {
+           $found=true;
+           break;
+       }
+    }
+    if(found){
+        foreach($majorList as $i){
+            if($i!=myMajor){
+                if(!array_key_exists($i, $majors){//if major isnt in the array, add it.
+                    $temp = new major();
+                    $temp.name = $i;
+                    $majors[$i]=$temp;
+                }
+                $majors[$i].courses[]=$row['courseID'];
+                $majors[$i].courses[]=$row['courseName'];
+            }
+        }
+    }
+}
+?>
